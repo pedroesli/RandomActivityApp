db7247f3c9400c490e206679bfa60ba10e0d3858
Pedro Esli <pedroesli.vn@gmail.com>
2021-08-30 18:16:32 -0300

    Update: Configuration View

---
+++ RandomActivityApp/RandomActivityApp/Controllers/ConfigurationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConfigurationViewController.swift
+//  RandomActivityApp
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 30/08/21.
+//
+
+import UIKit
+
+class ConfigurationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .dark
+        
+        title = "Filters"
+    }
+
+}
--- RandomActivityApp/RandomActivityApp/Controllers/ViewController.swift
+++ RandomActivityApp/RandomActivityApp/Controllers/ViewController.swift
@@ -11,8 +11,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        overrideUserInterfaceStyle = .dark
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    @IBAction func configurationButtonClicked(_ sender: UIButton) {
+        let sb = UIStoryboard(name: "Configuration", bundle: nil)
+        let vc =  sb.instantiateViewController(identifier: "ConfigurationView") as! ConfigurationViewController
+        
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
