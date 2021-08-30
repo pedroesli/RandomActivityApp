698f2074d2e3264a90f1bd215f0b4e35f91b49e3
Pedro Esli <pedroesli.vn@gmail.com>
2021-08-30 17:35:04 -0300

    Create: Tela principal do app

--- RandomActivityApp/RandomActivityApp/Controllers/ViewController.swift
+++ RandomActivityApp/RandomActivityApp/Controllers/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
